96b297b155ce9c8277507b3f19bc967b51becdb9
Luis Sotomayor <LSotomayor@zynx.com>
2013-05-20 22:35:38 -0700

    Added test and dependencies

---
+++ src/test/groovy/lasl321/spikes/test/HelloFromJavaTest.groovy
@@ -0,0 +1,11 @@
+package lasl321.spikes.test
+
+import lasl321.spikes.HelloFromJava
+import org.junit.Test;
+
+class HelloFromJavaTest {
+    @Test
+    void shouldDoSomething() {
+        new HelloFromJava()
+    }
+}
